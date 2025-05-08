3ea04910c917cbbce3804ae0981a04c4dd99ac85
Anders-Have <10632098@ucn.dk>
2025-05-08 12:06:32 +0200

    added: tableImpl
    + comments

--- src/database/TableImpl.java
+++ src/database/TableImpl.java
@@ -1,6 +1,20 @@
 package database;
 
+import model.Table;
+
+/**
+ * 
+ * @author Anders Trankjær
+ * @Version 2025/08/05/11:30 
+ */
 public interface TableImpl
 {
-
+	/**
+	 * this method creates a clone of the table that has a given tableCode. if no matching product
+	 * is found the method returns null. 
+	 * 
+	 * @param tableCode the code used in the search
+	 * @return a clone of a table that matches the search parameters
+	 */
+	Table findTableByCode(String tableCode);
 }
