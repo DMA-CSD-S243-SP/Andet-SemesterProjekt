0f6f1ea4e76018a909de4ed8de77f065955722c1
Anders-Have <10632098@ucn.dk>
2025-05-08 10:56:10 +0200

    added: table class

--- src/model/Table.java
+++ src/model/Table.java
@@ -1,6 +1,44 @@
 package model;
 
-public class Table
+public class Table 
 {
+	private int tableNumber;
+	private String tableCode;
+	private TableOrder currentTableOrder;
 
+	public Table(int tableNumber, String tableCode) 
+	{
+		this.tableNumber = tableNumber;
+		this.tableCode = tableCode;
+	}
+	
+	public int getTableNumber() 
+	{
+		return tableNumber;
+	}
+
+	public void setTableNumber(int tableNumber) 
+	{
+		this.tableNumber = tableNumber;
+	}
+
+	public String getTableCode() 
+	{
+		return tableCode;
+	}
+
+	public void setTableCode(int tableNumber, String restaurantCode) 
+	{
+		this.tableCode = tableNumber + restaurantCode;
+	}
+
+	public TableOrder getCurrentTableOrder() 
+	{
+		return currentTableOrder;
+	}
+
+	public void setCurrentTableOrder(TableOrder tableOrder) 
+	{
+		this.currentTableOrder = tableOrder;
+	}
 }
