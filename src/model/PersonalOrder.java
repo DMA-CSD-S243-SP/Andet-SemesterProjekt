9bae8d7bc83047831e28fd5de39ce75e99ab9e1c
Line-B-333 <10187764@ucn.dk>
2025-05-08 10:30:15 +0200

    Added: PersonalOrder attributes
    I've added attributes to the PersonalOrder class

--- src/model/PersonalOrder.java
+++ src/model/PersonalOrder.java
@@ -1,6 +1,27 @@
 package model;
 
+import java.util.List;
+
+/**
+ * @author Line Bertelsen
+ * @date 08.05.25 10.14
+ */
+
 public class PersonalOrder
 {
-
+	//Attributes/Instance variables
+	private int PersonalOrderId;
+	private double totalPersonalOrderPrice;
+	private String customerName;
+	private int customerAge;
+	
+	//List
+	private List<PersonalOrderLine> personalOrderLineList;
+	private List<SelectionOption> listOfSelectionsOption;
+	private List<AddOnOption> listOfAddOnOptions;
+	
+	public PersonalOrder(TableOrder tableOrder) 
+	{
+		
+	}
 }
