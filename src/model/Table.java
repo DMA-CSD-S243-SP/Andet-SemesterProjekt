c27cefc760b3c6369049e2b4bcd0bb00203efb4f
Anders-Have <10632098@ucn.dk>
2025-05-08 11:14:09 +0200

    added comments to table class

--- src/model/Table.java
+++ src/model/Table.java
@@ -1,5 +1,10 @@
 package model;
 
+/*
+ * The table class represents a physical table in
+ * @Author Anders Trankjær
+ * @Version 2025/08/05/10:00
+ */
 public class Table 
 {
 	private int tableNumber;
@@ -12,31 +17,52 @@ public class Table
 		this.tableCode = tableCode;
 	}
 	
+	/**
+	 * @return the tableNumber
+	 */
 	public int getTableNumber() 
 	{
 		return tableNumber;
 	}
 
+	/**
+	 * @param tableNumber the new tableNumber to set
+	 */
 	public void setTableNumber(int tableNumber) 
 	{
 		this.tableNumber = tableNumber;
 	}
 
+	/**
+	 * @return the tableCode
+	 */
 	public String getTableCode() 
 	{
 		return tableCode;
 	}
 
+	/**
+	 * the tableCode variable is made of two other variables tableNumber and restaurantCode
+	 * 
+	 * @param tableNumber a unique number for each table in a restaurant
+	 * @param restaurantCode a unique number that identifies a restaurant
+	 */
 	public void setTableCode(int tableNumber, String restaurantCode) 
 	{
 		this.tableCode = tableNumber + restaurantCode;
 	}
 
+	/**
+	 * @return the currentTableOrder associated with the table
+	 */
 	public TableOrder getCurrentTableOrder() 
 	{
 		return currentTableOrder;
 	}
 
+	/**
+	 * @param tableOrder the new tableOrder to set
+	 */
 	public void setCurrentTableOrder(TableOrder tableOrder) 
 	{
 		this.currentTableOrder = tableOrder;
