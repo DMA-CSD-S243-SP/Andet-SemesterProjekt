18233cd7851b40b264a538c3f712bca58b8f4f97
Anders-Have <10632098@ucn.dk>
2025-05-09 10:13:20 +0200

    added: MainCourse class
    + comments
    + small changes to drinks class

--- src/model/Drink.java
+++ src/model/Drink.java
@@ -1,10 +1,9 @@
 package model;
 
 /**
- * Represents a drink menu item that can be ordered at a bone's
- * restaurant.
+ * Represents a drink menu item that can be ordered at a bone's restaurant.
  * 
- * This class is a child/sub-class extending the abstract MenuItem class, and 
+ * This class is a child/sub-class extending the abstract MenuItem class, and
  * includes information on whether the item is a drink, along with whether the
  * drink is alcoholic and is refillable and the price.
  * 
@@ -12,74 +11,74 @@ package model;
  * @author Anders Trankjær
  * @version 09-05-2025 - 9:30
  */
-public class Drink extends MenuItem
+public class Drink extends MenuItem 
 {
-private boolean isAlcoholic;
-private boolean isRefill;
-private double price;
+	private boolean isAlcoholic;
+	private boolean isRefill;
+	private double price;
 
+	/**
+	 * the constructor for this subclass calls the constructor for the superclass to
+	 * initialize it.
+	 * 
+	 * @param isAlcoholic - whether the drink is alcoholic.
+	 * @param isRefill    - whether the drink is allowed to be refilled
+	 * @param price       - the price of the item
+	 */
+	public Drink(boolean isAlcoholic, boolean isRefill, double price) 
+	{
+		super();
 
-/**
- * the constructor for this subclass calls the constructor for the superclass to initialize it.
- * 
- * @param isAlcoholic - whether the drink is alcoholic.
- * @param isRefill - whether the drink is allowed to be refilled
- * @param price - the price of the item
- */
-public Drink(boolean isAlcoholic, boolean isRefill, double price)
-{
-	super();
-	
-	this.isAlcoholic = isAlcoholic;
-	this.isRefill = isRefill;
-	this.price = price;
-}
+		this.isAlcoholic = isAlcoholic;
+		this.isRefill = isRefill;
+		this.price = price;
+	}
 
-/**
- * @return isAlcoholic
- */
-public boolean isAlcoholic()
-{
-	return isAlcoholic;
-}
+	/**
+	 * @return isAlcoholic
+	 */
+	public boolean isAlcoholic() 
+	{
+		return isAlcoholic;
+	}
 
-/**
- * @param isAlcoholic the new isAlcoholic to be set
- */
-public void setAlcoholic(boolean isAlcoholic)
-{
-	this.isAlcoholic = isAlcoholic;
-}
+	/**
+	 * @param isAlcoholic the new isAlcoholic to be set
+	 */
+	public void setAlcoholic(boolean isAlcoholic) 
+	{
+		this.isAlcoholic = isAlcoholic;
+	}
 
-/**
- * @return isRefill
- */
-public boolean isRefill()
-{
-	return isRefill;
-}
+	/**
+	 * @return isRefill
+	 */
+	public boolean isRefill() 
+	{
+		return isRefill;
+	}
 
-/**
- * @param isRefill the new isRefill to be set
- */
-public void setRefill(boolean isRefill)
-{
-	this.isRefill = isRefill;
-}
+	/**
+	 * @param isRefill the new isRefill to be set
+	 */
+	public void setRefill(boolean isRefill) 
+	{
+		this.isRefill = isRefill;
+	}
 
-/**
- * @return price
- */
-public double getPrice()
-{
-	return price;
-}
+	/**
+	 * @return price
+	 */
+	public double getPrice() 
+	{
+		return price;
+	}
 
-/**
- * @param newPrice the new price to be set
- */
-public void setPrice(double newPrice)
-{
-	this.price = newPrice;
-}
+	/**
+	 * @param newPrice the new price to be set
+	 */
+	public void setPrice(double newPrice) 
+	{
+		this.price = newPrice;
+	}
 }
