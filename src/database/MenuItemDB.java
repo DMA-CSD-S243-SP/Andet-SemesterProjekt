209718b746941b8cea567f7d78dacd21c8b8f607
Line-B-333 <10187764@ucn.dk>
2025-05-11 21:11:09 +0200

    Update: MenuItemDB
    Added mehtod without function

--- src/database/MenuItemDB.java
+++ src/database/MenuItemDB.java
@@ -1,7 +1,10 @@
 package database;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import model.MenuItem;
 
 /**
  * This class is responsible for accessing and managing employee objects stored
@@ -10,10 +13,10 @@ import java.sql.SQLException;
  * It implements the employeeDaoImpl, meaning it implements its methods
  * 
  * @author Line Bertelsen
- * @version 11.05.2025 - 20:18
+ * @version 11.05.2025 - 21:10
  */
 
-public class MenuItemDB implements MenuCardImpl
+public class MenuItemDB implements MenuItemImpl
 {
 
 	// Selects a row from the table menuItem in the database, based on the given MenuItemID
@@ -52,18 +55,76 @@ public class MenuItemDB implements MenuCardImpl
 	 */
 	public MenuItemDB() throws SQLException
 	{
-		// Prepares the SQL statement for retrieving all employees
+		// Prepares the SQL statement for retrieving a MenuItem by its MenuItemId
 		statementFindMenuItemById = DataBaseConnection.getInstance().getConnection().prepareStatement(FIND_MENUITEM_BY_MENUITEMID_QUERY);
 		
-		// Prepares the SQL statement for retrieving an Employee by their employee id
+		// Prepares the SQL statement for retrieving a MultipleChoiceMenu by its MenuItemId
 		statementMultipleChoiceMenuByMenuItemId = DataBaseConnection.getInstance().getConnection().prepareStatement(FIND_MULTIPLECHOICEMENU_BY_MENUITEMID_QUERY);
 		
-		// Prepares the SQL statement for retrieving all employees
+		// Prepares the SQL statement for retrieving a SelcetionOption by its ChoiceMenuId
 		statementSelectionOptionByChoiceMenuId = DataBaseConnection.getInstance().getConnection().prepareStatement(FIND_SELECTIONOPTION_BY_CHOICEMENUID_QUERY);
 				
-		// Prepares the SQL statement for retrieving an Employee by their employee id
+		// Prepares the SQL statement for retrieving a AddOnOption by its MenuItemID
 		statementAddOnOptionByMenuItemId = DataBaseConnection.getInstance().getConnection().prepareStatement(FIND_ADDONOPTION_BY_MENUITEMID_QUERY);
 	}
 
+	
+	public MenuItem findMenuItemByMenuItemId(int menuItemId) throws SQLException
+	{
+		//TODO:
+		return null;	
+	}
 	 
+	private MenuItem buildMenuItemObject(ResultSet resultset) throws SQLException
+	{
+		return null;
+		//TODO:
+	}
+	
+	
+//	public MenuItem findMultipleChoiceMenuByMenuItemId(int menuItemId) throws SQLException
+//	{
+//		//TODO:
+//		return null;	
+//	}
+	 
+	private MenuItem buildMultipleChoiceMenuObject(ResultSet resultset) throws SQLException
+	{
+		return null;
+		//TODO:
+	}
+	
+//	public MenuItem findMenuItemByMenuItemId(int menuItemId) throws SQLException
+//	{
+//		//TODO:
+//		return null;	
+//	}
+	 
+	
+	private MenuItem buildSelectionOptionObject(ResultSet resultset) throws SQLException
+	{
+		return null;
+		//TODO:
+	}
+
+	
+//	public MenuItem findAddOnOptionByMenuItemId(int menuItemId) throws SQLException
+//	{
+//		//TODO:
+//		return null;	
+//	}
+	 
+	private MenuItem buildAddOnOptionObject(ResultSet resultset) throws SQLException
+	{
+		return null;
+		//TODO:
+	}
+
+
+	@Override
+	public MenuItem findMenuItemById(int menuItemId) throws DataAccessException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
