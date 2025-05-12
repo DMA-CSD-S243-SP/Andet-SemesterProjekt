006103c6e3c25b4c4080fd582b77542d8561625a
Line-B-333 <10187764@ucn.dk>
2025-05-12 17:32:00 +0200

    Created: TableOrderImpl

--- src/database/TableOrderImpl.java
+++ src/database/TableOrderImpl.java
@@ -1,6 +1,13 @@
 package database;
 
+import model.TableOrder;
+
+/**
+ * @author Line Bertelsen
+ * @version 12.05.25 - 17.30
+ */
+
 public interface TableOrderImpl
 {
-
+	TableOrder findTableOrderById(int TableOrderId) throws DataAccessException;
 }
