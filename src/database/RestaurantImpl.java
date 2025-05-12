881276af08aaf8bc2be81f21da0649458361cde5
Anders-Have <10632098@ucn.dk>
2025-05-12 12:57:27 +0200

    added: restaurantImpl
    + comments

--- src/database/RestaurantImpl.java
+++ src/database/RestaurantImpl.java
@@ -1,6 +1,29 @@
 package database;
 
+import java.util.List;
+
+import model.Restaurant;
+
+/**
+ * 
+ * @author Anders Trankjær
+ * @Version 2025/12/05/12:05 
+ */
 public interface RestaurantImpl
 {
-
+	/**
+	 * TODO
+	 * 
+	 * @return - a list of all restaurants in the database
+	 */
+	List<Restaurant> findAllRestaurants();
+	
+	/**
+	 * this method will search through the database using the search criteria from the parameterlist
+	 * make a clone of using the data found and return that. 
+	 * 
+	 * @param restaurantCode
+	 * @return
+	 */
+	Restaurant findRestaurantByCode(String restaurantCode);
 }
