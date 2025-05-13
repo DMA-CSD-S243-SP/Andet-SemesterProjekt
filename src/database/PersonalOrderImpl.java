dc71f1a87cf286d4f4ceb7bc87f22197fec23c51
Line-B-333 <10187764@ucn.dk>
2025-05-13 12:29:47 +0200

    Created: PersonalOrderImpl
    Created the method for the interface PersonalOrder

--- src/database/PersonalOrderImpl.java
+++ src/database/PersonalOrderImpl.java
@@ -1,6 +1,28 @@
 package database;
 
+import model.PersonalOrder;
+
+/**
+ * An interface that defines a contract for accessing PersonalOrderImpl,
+ * specifically for finding a PersonalOrder based on its ID.
+ * 
+ * The contract ensures consistency across all classes that implement the interface. 
+ * It guarantees that certain functionality is available 
+ * and any class that implements MenuItemImpl must provide 
+ * the methods called findPersonalOrderById .
+ * 
+ * @author Line Bertelsen
+ * @version 13.05.25 - 12.25
+ */
 public interface PersonalOrderImpl
 {
-
+	/**
+     * Method creates a shallow clone of the personalOrder that has the given unique ID. 
+     * If no match is found the method returns null. 
+     *
+     * @param personalOrderId the code of the personalOrder to be retrieved
+     * @return the PersonalOrder object that matches the provided ID
+     * @throws DataAccessException if there is an issue accessing the data
+     */
+	PersonalOrder findPersonalOrderById(int personalOrderId) throws DataAccessException;
 }
