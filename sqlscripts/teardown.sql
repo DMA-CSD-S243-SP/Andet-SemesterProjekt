dd1e0056d6c9c503c505a8f0ef13114fe30ff8d1
LumiUCN <10632106@ucn.dk>
2025-05-13 08:54:06 +0200

    Create teardown.sql

---
+++ sqlscripts/teardown.sql
@@ -0,0 +1,77 @@
+drop table [DiscountPersonalOrder];
+GO;
+drop table [Discount];
+GO;
+drop table [PersonalOrderLine];
+GO;
+drop table [PersonalOrder];
+GO;
+drop table [AvailabilityTracker];
+GO;
+drop table [SelectionOption];
+GO;
+drop table [MultipleChoiceMenu];
+GO;
+drop table [AddOnOption];
+GO;
+drop table [MainCourse];
+GO;
+drop table [DipsAndSauces];
+GO;
+drop table [SideDish];
+GO;
+drop table [Drink];
+GO;
+drop table [SelfServiceBar];
+GO;
+drop table [PotatoDish];
+GO;
+drop table [MenuItem];
+GO;
+drop table [MenuCard];
+GO;
+alter table [Object_Table] drop constraint [FK_Object_Table_TableOrder];
+GO;
+drop table [TableOrder];
+GO;
+drop table [Object_Table];
+GO;
+drop table [Restaurant];
+GO;
+drop table [Employee];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
