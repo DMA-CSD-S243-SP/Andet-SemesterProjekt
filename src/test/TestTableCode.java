f2d581278feb8349d4644b2315759358d94143aa
Christoffer <christoffer.soendergaard@outlook.com>
2025-05-18 15:28:14 +0200

    Fixed: TestTableCode
    Fixed the unit test called TestTableCode, by correcting the expected assertions along with adding some act arrange assert comments.

--- src/test/TestTableCode.java
+++ src/test/TestTableCode.java
@@ -7,29 +7,35 @@ import org.junit.jupiter.api.Test;
 import model.Restaurant;
 import model.Table;
 
+
 /**
  * This class tests the functionality of tableCode across the Restaurant and Table classes
  * 
- * @author Anders Have
- * @version 14/05/2025 - 12:20
+ * @author Anders Have & Christoffer Søndergaard
+ * @version 18/05/2025 - 15:23
  */
 public class TestTableCode 
 {
-	
 	@Test
 	public void testTableCode()
 	{
+		// Arrange
 		Table table1 = new Table(1, null);
 		Table table2 = new Table(2, null);
 		
 		Restaurant restaurant1 = new Restaurant("Bones", "Aalborg", "epicStreet");
-		restaurant1.setRestaurantCode("001");
 		
+		
+		// Act
+		restaurant1.setRestaurantCode("001");
+			
 		table1.setTableCode(table1.getTableNumber(), restaurant1.getRestaurantCode());
 		table2.setTableCode(table2.getTableNumber(), restaurant1.getRestaurantCode());
 	
-		// tableCode is a unique code made up of tableNumber and the restaurantCode of the restaurant it is within.
-		assertEquals("1001", table1.getTableCode());
-		assertEquals("2001", table2.getTableCode());
+	
+		// Assert
+		// tableCode is a unique code made up of tableNumber and the restaurantCode of the restaurant the table is within.
+		assertEquals("0001001", table1.getTableCode());
+		assertEquals("0002001", table2.getTableCode());
 	}
 }
