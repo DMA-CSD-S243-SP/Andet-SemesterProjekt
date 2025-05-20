37ff48afa911377f9f7d4fb9126cfb11678a1c70
Christoffer <christoffer.soendergaard@outlook.com>
2025-05-20 13:47:33 +0200

    Added: Class ViewGuestTableInformation
    Added the ViewGuestTableInformation which is the first view / window and the one that is being launched by the main method.

---
+++ src/gui/ViewGuestTableInformation.java
@@ -0,0 +1,116 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+/**
+ * TODO: Write a thorough description of this class and also java docs
+ * for the constructor and the class' methods
+ * 
+ * 
+ * @author Christoffer Søndergaard
+ * @version 20/05/2025 - 13:45
+ */	
+public class ViewGuestTableInformation extends JFrame
+{
+	// Added in order to suppress the warning that appears in serializable classes where no serialVersionUID is specified
+	private static final long serialVersionUID = 1L;
+	
+	// The Jframes that will be retrieved from the guest frame theme and used for manipulating the contents
+	private JPanel navigationPanel;
+	private JPanel primaryContentPanel;
+	private JPanel bottomPanel;
+	
+	// Determines whether or not this frame should have the "tilbage" button enabled in the navigational panel
+	boolean isPreviousViewEnabled = false;
+	
+	// Determines whether or not the 'Anmod om service' button is enabled in the navigational panel
+	boolean isServiceEnabled = false;
+	
+	
+	/**
+	 * Create the frame.
+	 */
+	public ViewGuestTableInformation()
+	{
+		initGUI();
+	}
+	
+	
+	private void initGUI()
+	{
+		// Creates a ComponentFrameThemeGuest component
+		ComponentFrameThemeGuest frameTheme = new ComponentFrameThemeGuest();
+
+		// Changes the frame / view's theme to the universal bone's brand theme
+		// - 	OBS! Since the heading and instruction text utilizes HTML for structuring
+		// 		you must use the <br> tag to break up the text instead of \n 
+		frameTheme.applyGeneralVisuals("BORDINFORMATION", this, "Indlæs Bordinformation", "Udfyld informationsboksen nedenfor med<br> indholdet som står på skiltet tilhørende dit bord");
+		
+		// Retrieves the navigationPanel component from the frameTheme
+		navigationPanel = frameTheme.getNavigationPanel();
+		
+		// Retrieves the primary content panel component from the the frameTheme
+		// - This is the panel that we add our varying contents to in the different view / windows
+		primaryContentPanel = frameTheme.getPrimaryContentPanel();
+		
+		// Retrieves the bottomContentPanel component from the frameTheme
+		bottomPanel = frameTheme.getBottomContentPanel();
+
+
+		
+		///////////////////////////////
+		// - Primary Content Panel - //
+		///////////////////////////////
+		//   THIS IS WHERE CONTENT   //
+		//   SHOULD BE INSERTED IN   //
+		///////////////////////////////
+		
+		
+		ComponentGuestInputField inputField = new ComponentGuestInputField("Bordnummer", "onlyNumbers");
+		frameTheme.getPrimaryContentPanel().add(inputField);
+		
+		
+		
+		////////////////////////////////
+		// - Navigation Back Button - //
+		////////////////////////////////
+
+		
+		
+		///////////////////////////////////
+		// - Navigation Service Button - //
+		///////////////////////////////////
+		
+
+		
+		//////////////////////////////
+		// - Bottom Panel Buttons - //
+		//////////////////////////////
+		
+		
+		// Creates a customized button component with the supplied information
+		ComponentGuestButtonContinue btnContinue = new ComponentGuestButtonContinue("Fortsæt", bottomPanel);
+		
+		// Adds the customized button to the panel
+		bottomPanel.add(btnContinue);
+	
+		// Adds an action listener for when the button is clicked
+		btnContinue.addActionListener(event ->
+		{
+			// Creates the new frame that should be opened when pressing the button
+			ViewGuestCustomerInformation nextView = new ViewGuestCustomerInformation();
+
+			// Sets the visibility to true turning the previous view / window visible
+			nextView.setVisible(true);
+			
+			// Closes the current frame/window
+			this.dispose();
+		});
+	}
+}
