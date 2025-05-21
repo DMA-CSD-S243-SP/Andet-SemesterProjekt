a6ae6b078b35542563c245ea4ddf4820a149c220
LumiUCN <10632106@ucn.dk>
2025-05-21 10:46:44 +0200

    Update UtilityGuestInformation.java
    Added enterTableCode and enterNameAndAge operations

--- src/gui/UtilityGuestInformation.java
+++ src/gui/UtilityGuestInformation.java
@@ -1,10 +1,18 @@
 package gui;
+import java.sql.SQLException;
+import java.util.List;
+
 import application.PersonalOrderController;
+import database.DataAccessException;
+import model.Discount;
+import model.Table;
 
 public class UtilityGuestInformation
 {
 	private static UtilityGuestInformation instance;
 	private PersonalOrderController personalOrderController;
+	private Table table;
+	private List<Discount> listOfDiscounts;
 
 	private UtilityGuestInformation()
 	{
@@ -23,5 +31,17 @@ public class UtilityGuestInformation
 	public PersonalOrderController getPersonalOrderController()
 	{
 		return personalOrderController;
+	}
+	
+	public Table enterTableCode(String tableNumber, String restaurantCode) throws DataAccessException, SQLException
+	{
+		table = personalOrderController.enterTableCode(tableNumber, restaurantCode);
+		return table;
+	}
+	
+	public List<Discount> enterNameAndAge(String customerName, int customerAge)
+	{
+		listOfDiscounts = personalOrderController.enterNameAndAge(customerName, customerAge);
+		return listOfDiscounts;
 	}
 }
