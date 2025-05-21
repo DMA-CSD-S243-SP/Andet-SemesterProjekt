975261b8b946a19fa8fb609356a274c8cf2da3f1
LumiUCN <10632106@ucn.dk>
2025-05-21 12:58:51 +0200

    Update UtilityGuestInformation.java
    Further increased information hoarding

--- src/gui/UtilityGuestInformation.java
+++ src/gui/UtilityGuestInformation.java
@@ -1,18 +1,26 @@
 package gui;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import application.PersonalOrderController;
 import database.DataAccessException;
 import model.Discount;
+import model.MainCourse;
+import model.MenuCard;
+import model.MenuItem;
+import model.PotatoDish;
 import model.Table;
+import model.TableOrder;
 
 public class UtilityGuestInformation
 {
 	private static UtilityGuestInformation instance;
 	private PersonalOrderController personalOrderController;
 	private Table table;
+	private TableOrder tableOrder;
 	private List<Discount> listOfDiscounts;
+	private List<MenuCard> listOfMenuCards;
 
 	private UtilityGuestInformation()
 	{
@@ -36,6 +44,7 @@ public class UtilityGuestInformation
 	public Table enterTableCode(String tableNumber, String restaurantCode) throws DataAccessException, SQLException
 	{
 		table = personalOrderController.enterTableCode(tableNumber, restaurantCode);
+		tableOrder = table.getCurrentTableOrder();
 		return table;
 	}
 	
@@ -43,5 +52,72 @@ public class UtilityGuestInformation
 	{
 		listOfDiscounts = personalOrderController.enterNameAndAge(customerName, customerAge);
 		return listOfDiscounts;
+	}
+	
+	public List<MenuCard> enterDiscounts(List<Discount> listOfDiscounts) throws DataAccessException, SQLException
+	{
+		listOfMenuCards = personalOrderController.enterDiscounts(listOfDiscounts);
+		return listOfMenuCards;
+	}
+	
+	public MenuCard getAdultMenu()
+	{
+		MenuCard adultMenu = null;
+		for (MenuCard menu: listOfMenuCards)
+		{
+			System.out.println(menu.getName());
+			if (menu.getName().contains("Voksen"))
+			{
+				adultMenu = menu;
+			}
+		}
+		if (adultMenu == null)
+		{
+			throw new NullPointerException("No adult menu was found");
+		}
+		return adultMenu;
+	}
+	
+	public boolean isLunchTime()
+	{
+		boolean isLunch = false;
+		if (16<(tableOrder.getTimeOfArrival().getHour()))
+		{
+			isLunch = true;
+		}
+		return isLunch;
+	}
+	
+	public List<MainCourse> getMainCourses(MenuCard menuCard)
+	{
+		List<MainCourse> mainCourses = new ArrayList<>();
+		List<MenuItem> menuItems = menuCard.getAvailableMenuItems();
+		for (MenuItem item: menuItems)
+		{
+			if (item instanceof MainCourse)
+			{
+				mainCourses.add((MainCourse)item);
+			}
+		}
+		return mainCourses;
+	}
+	
+	public List<PotatoDish> getPotatoDishes(MenuCard menuCard)
+	{
+		List<PotatoDish> potatoDishes = new ArrayList<>();
+		List<MenuItem> menuItems = menuCard.getAvailableMenuItems();
+		for (MenuItem item: menuItems)
+		{
+			if (item instanceof PotatoDish)
+			{
+				potatoDishes.add((PotatoDish)item);
+			}
+		}
+		return potatoDishes;
+	}
+	
+	public void enterMainCourse(MainCourse mainCourse)
+	{
+		personalOrderController.enterMainCourse(mainCourse);
 	}
 }
