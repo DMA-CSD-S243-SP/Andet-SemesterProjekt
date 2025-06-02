91cfb69d392a5859dc5d66a254003056c2af72a5
Line-B-333 <10187764@ucn.dk>
2025-06-02 16:09:46 +0200

    Added: TableImpl JavaDoc

--- src/database/TableImpl.java
+++ src/database/TableImpl.java
@@ -7,17 +7,17 @@ import model.Table;
 /**
  * 
  * @author Anders Trankjær & Christoffer Søndergaard
- * @version 18/05/2025 - 16:55
+ * @version 02/06/2025 - 13:54
  */
 public interface TableImpl
 {
 	/**
-	 * this method creates a clone of the table that has a given tableCode. if no matching table
+	 * This method creates a clone of the table that has a given tableCode. if no matching table
 	 * is found the method returns null. 
 	 * 
-	 * @param tableCode the code used in the search
-	 * @return a clone of a table that matches the search parameters
-	 * @throws SQLException 
-	 */
+	 * @param tableCode 	- the code used in the search
+	 * @return chosenTable 	- a clone of a table that matches the search parameters
+	 * @throws SQLException	- if a SQL operation fails
+     */
 	Table findTableByCode(String tableNumber, String restaurantCode) throws DataAccessException, SQLException;
 }
