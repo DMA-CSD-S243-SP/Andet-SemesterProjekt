d256bfbf6ffc4f5115e4848aff9dab6f60e6476a
Line-B-333 <10187764@ucn.dk>
2025-06-02 16:10:50 +0200

    Added: MenuItemImpl JavaDoc

--- src/database/MenuItemImpl.java
+++ src/database/MenuItemImpl.java
@@ -18,7 +18,7 @@ import model.SelectionOption;
  * findMenuItemByMenuItemId, findMultipleChoiceMenuByChoiceMenuId, findSelectionOptionByChoiceMenuId and findAddOnOptionByMenuItemId.
  * 
  * @author Line Bertelsen
- * @version 12.05.25 - 16.00
+ * @version 02/06/2025 - 16.00
  */
 
 public interface MenuItemImpl
@@ -27,43 +27,43 @@ public interface MenuItemImpl
      * Method creates a shallow clone of the menuItem that has the given unique ID. 
      * If no match is found the method returns null. 
      *
-     * @param menuItemId the ID of the menu item to be retrieved
-     * @return the MenuItem object that matches the provided ID
-     * @throws DataAccessException if there is an issue accessing the data
-	 * @throws SQLException 
-     */
+     * @param menuItemId 			- the ID of the menu item to be retrieved
+     * @return menuItem 			- the MenuItem object that matches the provided ID
+ 	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
+	 */
 	MenuItem findMenuItemByMenuItemId(int menuItemId) throws DataAccessException, SQLException;
 	
 	/**
-     * Method creates a shallow clone of the multipleChoiceMenu that has the given unique ID. 
+     * Method creates a list of shallow clones of the multipleChoiceMenu that has the given unique ID. 
      * If no match is found the method returns null. 
      *
-     * @param multipleChoiceMenu the ID of the multiple choice menu to be retrieved
-     * @return the MultipleChoiceMenu object that matches the provided ID
-     * @throws DataAccessException if there is an issue accessing the data
-	 * @throws SQLException 
-     */
+     * @param multipleChoiceMenu 	- the ID of the multiple choice menu to be retrieved
+     * @return multipleChoiceMenu 	- a list of MultipleChoiceMenu object that matches the provided ID
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
+	 */
 	List<MultipleChoiceMenu> findMultipleChoiceMenusByMainCourseId(int mainCourseId) throws DataAccessException, SQLException;
 	
 	/**
-     * Method creates a shallow clone of the selectionOption that has the given unique ID. 
+     * Method creates a list of shallow clones of the selectionOption that has the given unique ID. 
      * If no match is found the method returns null. 
      *
-     * @param selectionOption the ID of the menu item to be retrieved
-     * @return the SelectionOption object that matches the provided ID
-     * @throws DataAccessException if there is an issue accessing the data
-	 * @throws SQLException 
+     * @param selectionOption 		- the ID of the menu item to be retrieved
+     * @return selectionOption 		- a list of SelectionOption object that matches the provided ID
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
      */
 	List<SelectionOption> findSelectionOptionsByChoiceMenuId(int mainCourseId) throws DataAccessException, SQLException;
 	
 	/**
-     * Method creates a shallow clone of the addOnOption that has the given unique ID. 
+     * Method creates a list of shallow clones of the addOnOption that has the given unique ID. 
      * If no match is found the method returns null. 
      *
      * @param addOnOption the ID of the menu item to be retrieved
-     * @return the AddOnOption object that matches the provided ID
-     * @throws DataAccessException if there is an issue accessing the data
-	 * @throws SQLException 
+     * @return addOnOption 			- a list of AddOnOption object that matches the provided ID
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
      */
 	List<AddOnOption> findAddOnOptionsByMainCourseId(int mainCourseId) throws DataAccessException, SQLException;
 }
