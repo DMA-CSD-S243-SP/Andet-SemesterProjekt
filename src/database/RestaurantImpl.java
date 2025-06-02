ff3dba92018842f7285375b4df76ae9b4495e13f
Line-B-333 <10187764@ucn.dk>
2025-06-02 16:10:09 +0200

    Added: RestaurantImpl JavaDoc

--- src/database/RestaurantImpl.java
+++ src/database/RestaurantImpl.java
@@ -7,18 +7,18 @@ import model.Restaurant;
 /**
  * 
  * @author Anders Trankjær
- * @Version 2025/12/05/12:05 
+* @version 02/06/2025 - 14:19 
  */
 public interface RestaurantImpl
 {
 	/**
-	 * this method will search through the database using the search criteria from the parameterlist
+	 * This method will search through the database using the search criteria from the parameterlist
 	 * make a clone of using the data found and return that. 
 	 * 
-	 * @param restaurantCode
-	 * @return
-	 * @throws DataAccessException 
-	 * @throws SQLException 
+	 * @param restaurantCode		- the Restaurant object containing the restaurant code
+	 * @return restaurant 			- that match the restaurantCode 
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
 	 */
 	Restaurant findRestaurantByCode(String restaurantCode) throws DataAccessException, SQLException;
 }
