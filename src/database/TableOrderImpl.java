b808d3ef32c6358db45adf0a1021f24a3b16bf86
Line-B-333 <10187764@ucn.dk>
2025-06-02 16:09:14 +0200

    Added: TableOrderImpl JavaDoc

--- src/database/TableOrderImpl.java
+++ src/database/TableOrderImpl.java
@@ -15,7 +15,7 @@ import model.TableOrder;
  * findAllTableOrders and findTableOrderById
  * 
  * @author Line Bertelsen
- * @version 12.05.25 - 17.30
+ * @version 02/06/2025 - 13:54
  */
 
 public interface TableOrderImpl
@@ -28,10 +28,9 @@ public interface TableOrderImpl
 	 * information from the row that the TableOrders has a key to.
 	 * 
 	 * 
-	 * @return A list containing a shallow clone of every TableOrder in the
-	 *         database.
-	 * @throws DataAccessException if there is an issue accessing the data
-	 * @throws SQLException
+	 * @return TableOrderlist 		- A list containing a shallow clone of every TableOrder in the database.
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
 	 */
 	List<TableOrder> findAllTableOrders() throws DataAccessException, SQLException;
 
@@ -39,29 +38,29 @@ public interface TableOrderImpl
 	 * Method creates a shallow clone of the tableOrder that has the given unique
 	 * ID. If no match is found the method returns null.
 	 *
-	 * @param tableOrder the ID of the menu item to be retrieved
-	 * @return the TableOrder object that matches the provided ID
-	 * @throws DataAccessException if there is an issue accessing the data
-	 * @throws SQLException
+	 * @param tableOrder 			- the ID of the menu item to be retrieved
+	 * @return tableOrder 			- the TableOrder object that matches the provided ID
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
 	 */
 	TableOrder findTableOrderByTableOrderId(int tableOrderId) throws DataAccessException, SQLException;
 
 	/**
 	 * Method updates a given TableOrder in data storage. Doesn't add associations, just updates the specific TableOrderRow
 	 * 
-	 * @param tableOrder the TableOrder to be updated.
-	 * @throws DataAccessException if there is an issue accessing the data
-	 * @throws SQLException
+	 * @param tableOrder		 	- the TableOrder to be updated.
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
 	 */
 	void updateTableOrder(TableOrder tableOrder) throws DataAccessException, SQLException;
 
 	/**
 	 * Method gets all the TableOrders that the kitchen should be able to see.
 	 * 
-	 * @return A List containing every TableOrder where isSentToKitchen is set to
-	 *         true, and isTableOrderClosed is set to false
-	 * @throws DataAccessException
-	 * @throws SQLException
+	 * @return TableOrder list 		- A List containing every TableOrder where isSentToKitchen is set to
+	 *         					      true, and isTableOrderClosed is set to false
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
 	 */
 	List<TableOrder> findAllVisibleToKitchenTableOrders() throws DataAccessException, SQLException;
 }
