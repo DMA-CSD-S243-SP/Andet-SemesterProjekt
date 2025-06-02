abb3ad0d970b26c6fcf8cd93cf29ca8d490d9ede
Line-B-333 <10187764@ucn.dk>
2025-06-02 16:10:30 +0200

    Added: RestaurantImpl JavaDoc

--- src/database/PersonalOrderImpl.java
+++ src/database/PersonalOrderImpl.java
@@ -15,7 +15,7 @@ import model.PersonalOrder;
  * the methods called findPersonalOrderById .
  * 
  * @author Line Bertelsen
- * @version 13.05.25 - 12.25
+ * @version 02/06/2025 - 15:08
  */
 public interface PersonalOrderImpl
 {
@@ -23,23 +23,33 @@ public interface PersonalOrderImpl
      * Method creates a shallow clone of the personalOrder that has the given unique ID. 
      * If no match is found the method returns null. 
      *
-     * @param personalOrderId the code of the personalOrder to be retrieved
-     * @return the PersonalOrder object that matches the provided ID
-     * @throws DataAccessException if there is an issue accessing the data
-	 * @throws SQLException 
+     * @param personalOrderId  		- the code of the personalOrder to be retrieved
+     * @return personalOrder 		- the PersonalOrder object that matches the provided ID
+     * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+     * @throws SQLException			- if a SQL operation fails
      */
 	PersonalOrder findPersonalOrderById(int personalOrderId) throws DataAccessException, SQLException;
 
 
 	/**
-	 * The Method is used in FinishPersonalOrder in PersonalOrderController.
+	 * The method is used in ViewGuesTableOrderConfirmation in gui layer.
+	 * and the method FinishPersonalOrder in PersonalOrderController.
 	 * Inserts a new PersonalOrder into the PersonalOrder table in the Database. 
      *
-	 * @param personalOrder
-	 * @return
-	 * @throws DataAccessException
+	 * @param personalOrder			- the PersonalOrder object containing customer.
+	 * @param tableOrderId 			- the Id that belongs to this personal order
+	 * @return personalOrder		- that PersonalOrder object, now considered persisted in the database
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
 	 */
 	PersonalOrder insertPersonalOrder(PersonalOrder personalOrder, int tableOrderId) throws DataAccessException;
 	
+	/**
+	 * The method is use in ViewGuesTableOrder in gui layer
+	 * to find all personalOrders that is listed in the currentTableOrder
+	 * 
+	 * @param tableOrderId 			- the Id to find matching personalOrders
+	 * @throws DataAccessException 	- if an error occurs during data access, such as rollback or connection issues
+	 * @throws SQLException			- if a SQL operation fails
+	 */
 	List<PersonalOrder> findPersonalOrdersBytableOrderId (int TableOrderId) throws SQLException, DataAccessException;
 }
