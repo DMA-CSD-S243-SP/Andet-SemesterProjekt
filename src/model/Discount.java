0acf1ad1b86641558ea9088d30ee3df8750371d4
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-05 14:44:55 +0200

    Changed: Discount (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/model/Discount.java
+++ src/model/Discount.java
@@ -1,5 +1,19 @@
+// Packages
 package model;
 
+
+/**
+ * Represents a type of discount that can be applied to the customer's
+ * personal order. 
+ * 
+ * This class is not fully implemented at this point in the program's
+ * development, as it is a different use that has yet to be integrated
+ * in to the system.
+ * 
+ * 
+ * @author Christoffer Søndergaard
+ * @version 01/01/2025 - 15:09
+ */
 public class Discount
 {
 
