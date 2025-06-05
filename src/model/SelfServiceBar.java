918602444757eef51b96408555178812c6a83990
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-05 14:33:57 +0200

    Changed: SelfServiceBar (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/model/SelfServiceBar.java
+++ src/model/SelfServiceBar.java
@@ -3,6 +3,10 @@ package model;
 
 
 /**
+ * Represents a physical item from a Bone's restaurant menu, that is considered
+ * to be a self-service type of dish such as a soft-ice bar and salat bar that are
+ * ad libitum.
+ * 
  * Represents the item on the menu in one of Bone's many restaurants, that
  * allows for a guest to go to either a salad bar, or softice bar, where they
  * can eat as much as they would like to of the respective menu item that was
@@ -13,11 +17,12 @@ package model;
  * 
  * 
  * @author Christoffer Søndergaard
- * @version 08-05-2025 - 14:04
+ * @version 05/06/2025 - 14:31
  */
 public class SelfServiceBar extends MenuItem
 {
-    private EnumBarType barType;
+    // Used to determine the type of self service bar this is
+	private EnumBarType barType;
     
     private double lunchPrice;
     private double eveningPrice;
@@ -26,14 +31,14 @@ public class SelfServiceBar extends MenuItem
 	 * Constructs a new SelfServiceBar instance, which uses the specified parameters of
 	 * its' super-class / parent class MenuItem, along with its own unique parameters aswell.
 	 * 
-	 * @param barType the type of the self service bar this is, options are SALADBAR or SOFTICEBAR
-	 * @param lunchPrice the price of this item during the specified lunch hours
-	 * @param eveningPrice the price of this item during the specified evening hours
-	 * @param menuItemId 
-	 * @param preparationTime 
-	 * @param name 
-	 * @param description 
-	 * @param isMadeByKitchenStaff 
+	 * @param barType				- the type of the self service bar this is, options are SALADBAR or SOFTICEBAR
+	 * @param lunchPrice  			- the price of this item during the specified lunch hours
+	 * @param eveningPrice 			- the price of this item during the specified evening hours
+	 * @param menuItemId 			- the unique id of the MenuItem
+	 * @param preparationTime 		- the time in seconds it takes to prepare this MenuItem
+	 * @param name 					- the name of the MenuItem
+	 * @param description 			- the description of the MenuItem
+	 * @param isMadeByKitchenStaff	- whether this MenuItem is something the kitchen should handle or not
 	 */
     public SelfServiceBar(EnumBarType barType, double lunchPrice, double eveningPrice, int menuItemId, int preparationTime, String name, String description, boolean isMadeByKitchenStaff)
     {
@@ -50,6 +55,13 @@ public class SelfServiceBar extends MenuItem
 
     /**
      * Gets the type of self service bar this instance is.
+     * 
+     * This method is used to determine the category of the
+     * self service bar item in question, in Bone's current business
+     * model it can be either SALADBAR to SOFTICEBAR, but to future
+     * proof the application an enumerator is being used, in case Bone's
+     * wants to implement a third type of self-service then it is easy
+     * to implement.
      *
      * @return the bar type as defined in the EnumBarType class
      */
@@ -62,8 +74,12 @@ public class SelfServiceBar extends MenuItem
     /**
      * Sets the type of the self service bar this instance is.
      *
-     * This method is used to change the category of the self service
-     * bar item, for example from SALADBAR to SOFTICEBAR.
+     * This method is used to determine the category of the
+     * self service bar item in question, in Bone's current business
+     * model it can be either SALADBAR to SOFTICEBAR, but to future
+     * proof the application an enumerator is being used, in case Bone's
+     * wants to implement a third type of self-service then it is easy
+     * to implement.
      *
      * @param barType followed by the new type of bar
      */
@@ -75,6 +91,9 @@ public class SelfServiceBar extends MenuItem
 
     /**
      * Gets the price of of this item during the lunch time period.
+     *
+     * Allow for the restaurant to change the prices based on whether
+     * the time of day is in the restaurant's defined lunch hours.
      *
      * @return the lunch price of this item
      */
@@ -100,6 +119,9 @@ public class SelfServiceBar extends MenuItem
 
     /**
      * Gets the price of this item during the evening time period.
+     * 
+     * Allow for the restaurant to change the prices based on whether
+     * the time of day is in the restaurant's defined evening hours.
      *
      * @return the evening price of this item
      */
