4851d61a460c4fcda37f08458e9a89901cfb61a8
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-05 14:41:41 +0200

    Changed: EnumBarType (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/model/EnumBarType.java
+++ src/model/EnumBarType.java
@@ -5,13 +5,14 @@ package model;
 /**
  * Represents the different available types of self-service bars
  * at any of Bone's restaurants. It is noteworthy that this could have
- * been done using a boolean check, but to account for future use of
- * the system we chose to use an enum, in case the restaurant chooses
- * to open for a new self-service bar solution.
+ * been done using a boolean check in the program's current state,
+ * but to account for future usage and development of the system it was
+ * chosen to use an enumerator, in case the restaurant chooses to add
+ * a new type of self-service bar to their menu.
  * 
  * 
  * @author Christoffer Søndergaard
- * @version 08-05-2025 - 13:34
+ * @version 01/03/2025 - 13:34
  */
 public enum EnumBarType
 {
