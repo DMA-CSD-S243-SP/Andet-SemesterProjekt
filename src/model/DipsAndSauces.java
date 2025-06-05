0d3b98fef2f978dde58df1193f3978dfda97df07
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-05 14:38:53 +0200

    Changed: DipsAndSauces (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/model/DipsAndSauces.java
+++ src/model/DipsAndSauces.java
@@ -12,11 +12,15 @@ package model;
  * 
  * 
  * @author Christoffer Søndergaard
- * @version 12-05-2025 - 15:09
+ * @version 05/06/2025 - 11:09
  */
 public class DipsAndSauces extends MenuItem
 {
+	// Determines if the DipsAndSauces instance is a dip or a sauce
     private boolean isSauce;
+    
+	// This price is the one that is being used regardless of whether
+	// the getEveningPrice or getLunchPrice is being called upon
     private double fixedPrice;
 
 
@@ -25,13 +29,13 @@ public class DipsAndSauces extends MenuItem
 	 * its' super-class / parent class MenuItem, along with its own unique parameters aswell.
 	 * This initializes the item as either a dip or a sauce and sets the price.
      * 
-     * @param isSauce true if this item is a sauce, else false if the item is a dip
-     * @param fixedPrice the constant price of the item regardless of which type of dip, sauce or time of day it is
-     * @param menuItemId 
-     * @param preparationTime 
-     * @param name 
-     * @param description 
-     * @param isMadeByKitchenStaff 
+     * @param isSauce 				- determines if this item is a sauce (true), else false if the item is a dip
+     * @param fixedPrice 			- the constant price of the item regardless of which type of dip, sauce or time of day it is
+	 * @param menuItemId 			- the unique id of the MenuItem
+	 * @param preparationTime 		- the time in seconds it takes to prepare this MenuItem
+	 * @param name 					- the name of the MenuItem
+	 * @param description 			- the description of the MenuItem
+	 * @param isMadeByKitchenStaff	- whether this MenuItem is something the kitchen should handle or not
      */
     public DipsAndSauces(boolean isSauce, double fixedPrice, int menuItemId, int preparationTime, String name, String description, boolean isMadeByKitchenStaff)
     {
@@ -46,7 +50,11 @@ public class DipsAndSauces extends MenuItem
 
 
     /**
-     * Indicates whether this item is considered to be a sauce.
+     * Returns whether or not this item is a sauce or dip.
+     * 
+     * Retrieves whether this item is considered to be a sauce or a dip.
+     * If it returns true this object is perceived as a sauce, elsewise
+     * returns false when it is a dip.
      *
      * @return true if it is a sauce; false if it is a dip
      */
@@ -57,7 +65,10 @@ public class DipsAndSauces extends MenuItem
 
 
     /**
-     * Sets whether this item is considered to be a sauce.
+     * Sets whether this item is considered to be a sauce or a dip.
+     * 
+     * If it is set to true, this instance is perceived as a sauce, 
+     * elsewise set it to false to indicate that it is a dip.
      *
      * @param isSauce true if the item is a sauce; false if it is a dip
      */
@@ -67,22 +78,36 @@ public class DipsAndSauces extends MenuItem
     }
 
 
-    /**
-     * Returns the fixed price of the dip or sauce item.
-     *
-     * @return the price assigned to the item
-     */
+	/**
+	 * Returns the value of the variable fixedPrice.
+	 * 
+	 * The fixedPrice attribute / instance variable, determines the price
+	 * for this particular Sauce or Dip, it is called fixedPrice to emphasize
+	 * that all Sauces in Bone's current business model use the same
+	 * price regardless of their flavor or type. The only varying is that
+	 * a dip costs less than a price, but similar to the sauces all dips
+	 * costs the same regardless of their flavor or type.
+	 * 
+	 * @return the price of this DipsAndSauces instance
+	 */
     public double getFixedPrice()
     {
         return this.fixedPrice;
     }
 
 
-    /**
-     * Sets the fixed price of the dip or sauce item.
-     *
-     * @param fixedPrice the new fixed price to apply
-     */
+	/**
+	 * Sets the value of the variable fixedPrice.
+	 * 
+	 * The fixedPrice attribute / instance variable, determines the price
+	 * for this particular Sauce or Dip, it is called fixedPrice to emphasize
+	 * that all Sauces in Bone's current business model use the same
+	 * price regardless of their flavor or type. The only varying is that
+	 * a dip costs less than a price, but similar to the sauces all dips
+	 * costs the same regardless of their flavor or type.
+	 * 
+	 * @param fixedPrice the price to assign to this instance of DipsAndSauces
+	 */
     public void setFixedPrice(double fixedPrice)
     {
     	this.fixedPrice = fixedPrice;
@@ -91,7 +116,11 @@ public class DipsAndSauces extends MenuItem
     
 	/**
 	 * Gets the lunch price of this DipsAndSauces instance.
-	 * This method essentially retrieves the value of the instance's fixedPrice attribute.
+	 * 
+	 * This method essentially retrieves the value of the instance's fixedPrice attribute
+	 * as Bone's current business model does not have varying prices for evening or lunch
+	 * periods for this particular menu item, but to future proof the application this has
+	 * been added.
 	 *
 	 * @return the price of purchasing this DipsAndSauces during lunch time hours
 	 */
@@ -101,10 +130,14 @@ public class DipsAndSauces extends MenuItem
 		return this.fixedPrice;
 	}
 
-
+	
 	/**
 	 * Gets the evening price of this DipsAndSauces instance.
-	 * This method essentially retrieves the value of the instance's fixedPrice attribute.
+	 * 
+	 * This method essentially retrieves the value of the instance's fixedPrice attribute
+	 * as Bone's current business model does not have varying prices for evening or lunch
+	 * periods for this particular menu item, but to future proof the application this has
+	 * been added.
 	 *
 	 * @return the price of purchasing this DipsAndSauces during evening time hours
 	 */
