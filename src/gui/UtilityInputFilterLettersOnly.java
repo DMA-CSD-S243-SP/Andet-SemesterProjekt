c09235b65be71af9e75362e091c56223b6deaa95
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-05 15:12:56 +0200

    Changed: UtilityInputFilterLettersOnly(Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/gui/UtilityInputFilterLettersOnly.java
+++ src/gui/UtilityInputFilterLettersOnly.java
@@ -8,36 +8,45 @@ import javax.swing.text.DocumentFilter;
 
 
 /**
- * TODO: Write a thorough description of this class and also java docs
- * for the constructor and the class' methods
+ * Serves as a customized input filter that can be applied to GUI components
+ * that are either JTextField or extensions of the JTextField class.
+ * 
+ * This class allows for an input filter to be applied to the JTextField input
+ * fields, that makes it so the registered inputs will only be added to the
+ * input field if the inputted contents are either letters ranging from
+ * A to Z including ÆØÅ both capital and non-capital letters are accepted.
  * 
  * 
  * @author Christoffer Søndergaard
- * @version 20/05/2025 - 13:19
+ * @version 05/06/2025 - 15:11
  */	
 public class UtilityInputFilterLettersOnly extends DocumentFilter
 {
 	// Disables the filter initially
-	private boolean active = false;
+	private boolean isFilterActive = false;
 	
 	// The regex pattern that we use, which only allows matches for the following:
 	// small and capital letters from a to z and also including æ, ø and å
 	private static final Pattern LETTERS_ONLY_PATTERN = Pattern.compile("[a-zA-ZÆØÅæøå]+");
 
-	
-	public void setActive(boolean active)
+	// Determines whether or not the filtering is currently active to deactivate it
+	// before the user types to allow for the placeholder text to be there
+	public void setActive(boolean isFilterActive)
 	{
 		// Enables or disables the filtering
-		this.active = active;
+		this.isFilterActive = isFilterActive;
 	}
 
 	
+	// Overrides the methods with the same name in the DocumentFilter super class 
+	// allowing for adding additional validation and catching the user's input 
+	// upon inserting information and preventing certain things from being added  
 	@Override
 	public void insertString(FilterBypass filterByPass, int offset, String textToFilter, AttributeSet setOfAttributes) throws BadLocationException
 	{
 		// If the filter is inactive, or if the the text matches the regex pattern
 		// Pattern - Only include small and capital letters from a to z, no spaces, and æ,ø and å
-		if(!active || textToFilter != null && LETTERS_ONLY_PATTERN.matcher(textToFilter).matches())
+		if(!isFilterActive || textToFilter != null && LETTERS_ONLY_PATTERN.matcher(textToFilter).matches())
 		{
 			// Replaces the supplied input
 			super.insertString(filterByPass, offset, textToFilter, setOfAttributes);
@@ -45,12 +54,15 @@ public class UtilityInputFilterLettersOnly extends DocumentFilter
 	}
 
 	
+	// Overrides the methods with the same name in the DocumentFilter super class 
+	// allowing for adding additional validation and catching the user's input 
+	// upon replacing information and preventing certain things from being added
 	@Override
 	public void replace(FilterBypass filterByPass, int offset, int length, String textToFilter, AttributeSet setOfAttributes) throws BadLocationException
 	{
 		// If the filter is inactive, or if the the text matches the regex pattern
 		// Pattern - Only include small and capital letters from a to z, no spaces, and æ,ø and å
-		if (!active || textToFilter != null && LETTERS_ONLY_PATTERN.matcher(textToFilter).matches())
+		if (!isFilterActive || textToFilter != null && LETTERS_ONLY_PATTERN.matcher(textToFilter).matches())
 		{
 			// Replaces the supplied input
 			super.replace(filterByPass, offset, length, textToFilter, setOfAttributes);
