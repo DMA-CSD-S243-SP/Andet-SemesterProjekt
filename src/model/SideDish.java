3279f0902818be688bed3774cfe36b63b27f4477
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-05 14:33:35 +0200

    Changed: SideDish (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/model/SideDish.java
+++ src/model/SideDish.java
@@ -1,4 +1,6 @@
+// Packages
 package model;
+
 
 /**
  * Represents a phyiscal item from a Bone's restaurant menu, that is considered
@@ -11,12 +13,15 @@ package model;
  * side dish is made up of.
  * 
  * @author Line Bertelsen & Christoffer Søndergaard
- * @version 29/05/2025 - 12:57
+ * @version 05/06/2025 - 10:42
  */
 public class SideDish extends MenuItem
 {
 	// Instance variables
 	private int quantiyPerServing;
+	
+	// This price is the one that is being used regardless of whether
+	// the getEveningPrice or getLunchPrice is being called upon
 	private double fixedPrice;
 
 	
@@ -25,28 +30,31 @@ public class SideDish extends MenuItem
 	 * its' super-class / parent class MenuItem, along with its own unique parameters aswell.
 	 * This initializes the item having quantity per serving and a fixed price.
 	 * 
-     * @param fixedPrice the constant price of that specific side dish category regardless of the time of day it is.
-     * @param quantiyPerServing the quantity per serving to assign to this instance of SideDish 
-	 * @param menuItemId 
-	 * @param preparationTime 
-	 * @param name 
-	 * @param description 
-	 * @param isMadeByKitchenStaff 
+     * @param fixedPrice 			- the constant price of that specific side dish category regardless of the time of day it is.
+     * @param quantiyPerServing 	- the quantity per serving to assign to this instance of SideDish 
+	 * @param menuItemId 			- the unique id of the MenuItem
+	 * @param preparationTime 		- the time in seconds it takes to prepare this MenuItem
+	 * @param name 					- the name of the MenuItem
+	 * @param description 			- the description of the MenuItem
+	 * @param isMadeByKitchenStaff	- whether this MenuItem is something the kitchen should handle or not
      */
 	public SideDish(int quantiyPerServing, double fixedPrice, int menuItemId, int preparationTime, String name, String description, boolean isMadeByKitchenStaff)
 	{
-		// Calls the superclass constructor (MenuItem) to initialize the inherited attributes.
+		// Calls the super-class / parent class constructor (MenuItem) to initialize the inherited attributes
+		// which makes it so everything defined in the MenuItem class is initialized before this class' attribute values are set.
         super(menuItemId, preparationTime, name, description, isMadeByKitchenStaff);
 		
         // Changes the quantity per serving instance variable to what is specified the parameter list
 		this.quantiyPerServing = quantiyPerServing;
+		
 		this.fixedPrice = fixedPrice;
 	}
 	
 
 	/**
-	 * This method returns the value of the SideDish's instance variable
-	 * quantityPerServing, which determines how many 'pieces' a side dish
+	 * Returns the value of the SideDish's instance quantityPerServing variable.
+	 * 
+	 * The quantityPerServing determines how many 'pieces' a side dish
 	 * consists of e.g. a SideDish consisting of HotWings would consist of
 	 * 6 hotwings and not just a single one.
 	 * The quantityPerServing instance variable dictates how many of the 
@@ -62,8 +70,9 @@ public class SideDish extends MenuItem
 	
 	
 	/**
-	 * This method sets the value of the SideDish's instance variable
-	 * quantityPerServing, which determines how many 'pieces' a side dish
+	 * Sets the value of the SideDish's instance quantityPerServing variable
+	 * 
+	 * The quantityPerServing determines how many 'pieces' a side dish
 	 * consists of e.g. a SideDish consisting of HotWings would consist of
 	 * 6 hotwings and not just a single one.
 	 * The quantityPerServing instance variable dictates how many of the 
@@ -80,7 +89,8 @@ public class SideDish extends MenuItem
 	
 	
 	/**
-	 * The get method returns the value of the variable fixedPrice.
+	 * Returns the value of the variable fixedPrice.
+	 * 
 	 * The fixedPrice attribute / instance variable, determines the price
 	 * for this particular SideDish, it is called fixedPrice to emphasize
 	 * that all SideDishes in Bone's current business model use the same
@@ -95,7 +105,8 @@ public class SideDish extends MenuItem
 	
 	
 	/**
-	 * This method sets the value of the variable fixedPrice.
+	 * Sets the value of the variable fixedPrice.
+	 * 
 	 * The fixedPrice attribute / instance variable, determines the price
 	 * for this particular SideDish, it is called fixedPrice to emphasize
 	 * that all SideDishes in Bone's current business model use the same
@@ -111,6 +122,7 @@ public class SideDish extends MenuItem
 	
 	/**
 	 * Gets the lunch price of this SideDish instance.
+	 * 
 	 * This method essentially retrieves the value of the instance's fixedPrice attribute
 	 * as Bone's current business model does not have varying prices for evening or lunch
 	 * periods for this particular menu item, but to future proof the application this has
@@ -127,6 +139,7 @@ public class SideDish extends MenuItem
 
 	/**
 	 * Gets the evening price of this SideDish instance.
+	 * 
 	 * This method essentially retrieves the value of the instance's fixedPrice attribute
 	 * as Bone's current business model does not have varying prices for evening or lunch
 	 * periods for this particular menu item, but to future proof the application this has
