d68c1c6465d8c0c22d849b371883c98cd4eedc42
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-05 14:34:19 +0200

    Changed: PotatoDish (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/model/PotatoDish.java
+++ src/model/PotatoDish.java
@@ -14,11 +14,14 @@ package model;
  * 
  * 
  * @author Christoffer Søndergaard
- * @version 12-05-2025 - 15:12
+ * @version 05/06/2025 - 13:42
  */
 public class PotatoDish extends MenuItem
 {
     private boolean isPremium;
+    
+	// This price is the one that is being used regardless of whether
+	// the getEveningPrice or getLunchPrice is being called upon
     private double fixedPrice;
 
     
@@ -27,26 +30,35 @@ public class PotatoDish extends MenuItem
 	 * its' super-class / parent class MenuItem, along with its own unique parameters aswell.
 	 * This initializes the item as either a premium or a regular potato and sets the price.
      * 
-     * @param isPremium true if the dish is a premium potato based dish, else false if it is a regular potato-based dish
-     * @param fixedPrice the constant price of that specific potato dish category regardless of the time of day it is
-     * @param menuItemId 
-     * @param preparationTime 
-     * @param name 
-     * @param description 
-     * @param isMadeByKitchenStaff 
+     * @param isPremium 			- determines if the dish is a premium potato based dish (true), else false if it is a regular potato-based dish
+     * @param fixedPrice 			- the constant price of that specific potato dish category regardless of the time of day it is
+	 * @param menuItemId 			- the unique id of the MenuItem
+	 * @param preparationTime 		- the time in seconds it takes to prepare this MenuItem
+	 * @param name 					- the name of the MenuItem
+	 * @param description 			- the description of the MenuItem
+	 * @param isMadeByKitchenStaff	- whether this MenuItem is something the kitchen should handle or not
      */
     public PotatoDish(boolean isPremium, double fixedPrice, int menuItemId, int preparationTime, String name, String description, boolean isMadeByKitchenStaff)
     {
-        // Calls the superclass constructor (MenuItem) to initialize inherited attributes.
+		// Calls the super-class / parent class constructor (MenuItem) to initialize the inherited attributes
+		// which makes it so everything defined in the MenuItem class is initialized before this class' attribute values are set.
         super(menuItemId, preparationTime, name, description, isMadeByKitchenStaff);
 
         this.isPremium = isPremium;
+        
+    	// This price is the one that is being used regardless of whether
+    	// the getEveningPrice or getLunchPrice is being called upon
         this.fixedPrice = fixedPrice;
     }
 
 
     /**
-     * Returns whether this is a regular potato dish or if it is a premium and thereby a higher cost potato dish.
+     * Returns whether this is a regular potato dish or if it is perceived
+     * as a premium potato dish and thereby a higher cost potato dish.
+     * 
+     * In Bone's current business model the potato dishes are seperated in 
+     * to two groups, each group has a fixed price that differs from the other
+     * hence the potato dishes are divided in to premium and non-premium groups.
      *
      * @return true if the dish is considered 'premium' else returns false
      */
@@ -57,9 +69,15 @@ public class PotatoDish extends MenuItem
 
 
     /**
-     * Sets whether this potato dish is a regular potato dish or if it is a premium and higher cost potato dish.
+     * Sets whether this is a regular potato dish or if it is perceived
+     * as a premium potato dish and thereby a higher cost potato dish.
+     * 
+     * In Bone's current business model the potato dishes are seperated in 
+     * to two groups, each group has a fixed price that differs from the other
+     * hence the potato dishes are divided in to premium and non-premium groups.
      *
-     * @param isPremium true if this is perceived as a premium potato dish, else set to false for a standard/regular potato dish
+     * @param isPremium true if this is perceived as a premium potato dish,
+     * else set to false for a standard/regular potato dish
      */
     public void setPremiumPotatoDish(boolean isPremium)
     {
@@ -68,21 +86,35 @@ public class PotatoDish extends MenuItem
 
 
     /**
-     * Returns the fixed price of this PotatoDish instance.
-     *
-     * @return the price of this PotatoDish instance
+	 * Returns the value of the variable fixedPrice.
+	 * 
+	 * The fixedPrice attribute / instance variable, determines the price
+	 * for this particular PotatoDish, it is called fixedPrice to emphasize
+	 * that all PotatoDishes in the non-premium group uses the same price
+	 * and all the PotatoDishes in the premium group uses the same price
+	 * in Bone's current business regardless of the contents of the PotatoDish
+	 * or time of day.
+	 * 
+	 * @return the price of this PotatoDish instance
      */
     public double getFixedPrice()
     {
         return this.fixedPrice;
     }
 
-
-    /**
-     * Sets the fixed price of this PotatoDish instance.
-     *
-     * @param fixedPrice the price to assign to this instance of PotatoDish
-     */
+    
+	/**
+	 * Sets the value of the variable fixedPrice.
+	 * 
+	 * The fixedPrice attribute / instance variable, determines the price
+	 * for this particular PotatoDish, it is called fixedPrice to emphasize
+	 * that all PotatoDishes in the non-premium group uses the same price
+	 * and all the PotatoDishes in the premium group uses the same price
+	 * in Bone's current business regardless of the contents of the PotatoDish
+	 * or time of day.
+	 * 
+	 * @param fixedPrice the price to assign to this instance of PotatoDish
+	 */
     public void setFixedPrice(double fixedPrice)
     {
         this.fixedPrice = fixedPrice;
@@ -91,7 +123,11 @@ public class PotatoDish extends MenuItem
     
 	/**
 	 * Gets the lunch price of this PotatoDish instance.
-	 * This method essentially retrieves the value of the instance's fixedPrice attribute.
+	 * 
+	 * This method essentially retrieves the value of the instance's fixedPrice attribute
+	 * as Bone's current business model does not have varying prices for evening or lunch
+	 * periods for this particular menu item, but to future proof the application this has
+	 * been added.
 	 *
 	 * @return the price of purchasing this PotatoDish during lunch time hours
 	 */
@@ -104,7 +140,11 @@ public class PotatoDish extends MenuItem
 
 	/**
 	 * Gets the evening price of this PotatoDish instance.
-	 * This method essentially retrieves the value of the instance's fixedPrice attribute.
+	 * 
+	 * This method essentially retrieves the value of the instance's fixedPrice attribute
+	 * as Bone's current business model does not have varying prices for evening or lunch
+	 * periods for this particular menu item, but to future proof the application this has
+	 * been added.
 	 *
 	 * @return the price of purchasing this PotatoDish during evening time hours
 	 */
