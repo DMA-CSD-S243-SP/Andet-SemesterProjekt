56c6da9a05e487ceb937f1b94e51f8b036b304f4
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-05 15:22:00 +0200

    Changed: UtilityInputFilterNumbersOnly (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/gui/UtilityInputFilterNumbersOnly.java
+++ src/gui/UtilityInputFilterNumbersOnly.java
@@ -1,43 +1,54 @@
+// Packages
 package gui;
 
+// Imports
 import java.util.regex.Pattern;
-
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
 
 
 /**
- * TODO: Write a thorough description of this class and also java docs
- * for the constructor and the class' methods
+ * Serves as a customized input filter that can be applied to GUI components
+ * that are either JTextField or extensions of the JTextField class.
+ * 
+ * This class allows for an input filter to be applied to the JTextField input
+ * fields, that makes it so the registered inputs will only be added to the
+ * input field if the inputted contents are numbers in the form of numeric values
+ * ranging from 0 to 9.
  * 
  * 
  * @author Christoffer Søndergaard
- * @version 20/05/2025 - 13:19
+ * @version 05/06/2025 - 15:15
  */	
 public class UtilityInputFilterNumbersOnly extends DocumentFilter
 {
 	// Disables the filter initially
-	private boolean active = false;
+	private boolean isFilterActive = false;
 
 	// The regex pattern that we use, which only allows matches for the following:
 	// small and capital letters from a to z and also including æ, ø and å
 	private static final Pattern NUMBERS_ONLY_PATTERN = Pattern.compile("[0-9]+");
 
 	
-	public void setActive(boolean active)
+	// Determines whether or not the filtering is currently active to deactivate it
+	// before the user types to allow for the placeholder text to be there
+	public void setActive(boolean isFilterActive)
 	{
 		// Enables or disables the filtering
-		this.active = active;
+		this.isFilterActive = isFilterActive;
 	}
 
 	
+	// Overrides the methods with the same name in the DocumentFilter super class 
+	// allowing for adding additional validation and catching the user's input 
+	// upon inserting information and preventing certain things from being added  
 	@Override
 	public void insertString(FilterBypass filterByPass, int offset, String textToFilter, AttributeSet setOfAttributes) throws BadLocationException
 	{
 		// If the filter is inactive, or if the the text matches the regex pattern
-		// pattern - Only include numbers from 0 to 9 and no spaces
-		if (!active || textToFilter != null && NUMBERS_ONLY_PATTERN.matcher(textToFilter).matches())
+		// Pattern - Only include numbers from 0 to 9 and no spaces
+		if (!isFilterActive || textToFilter != null && NUMBERS_ONLY_PATTERN.matcher(textToFilter).matches())
 		{
 			// Replaces the supplied input
 			super.insertString(filterByPass, offset, textToFilter, setOfAttributes);
@@ -45,12 +56,15 @@ public class UtilityInputFilterNumbersOnly extends DocumentFilter
 	}
 
 	
+	// Overrides the methods with the same name in the DocumentFilter super class 
+	// allowing for adding additional validation and catching the user's input 
+	// upon replacing information and preventing certain things from being added
 	@Override
 	public void replace(FilterBypass filterByPass, int offset, int length, String textToFilter, AttributeSet setOfAttributes) throws BadLocationException
 	{
 		// If the filter is inactive, or if the the text matches the regex pattern
 		// pattern - Only include numbers from 0 to 9 and no spaces
-		if (!active || textToFilter != null && NUMBERS_ONLY_PATTERN.matcher(textToFilter).matches())
+		if (!isFilterActive || textToFilter != null && NUMBERS_ONLY_PATTERN.matcher(textToFilter).matches())
 		{
 			// Replaces the supplied input
 			super.replace(filterByPass, offset, length, textToFilter, setOfAttributes);
