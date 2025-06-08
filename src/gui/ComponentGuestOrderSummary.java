2110e3c8bd4acf193ffb053fd52b05c93be1308b
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-08 17:36:30 +0200

    Changed: ComponentGuestOrderSummary (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/gui/ComponentGuestOrderSummary.java
+++ src/gui/ComponentGuestOrderSummary.java
@@ -1,21 +1,43 @@
 package gui;
 
-import javax.swing.*;
-import java.awt.*;
+// Imports
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 
 /**
- * TODO: Write a thorough description of this class and also java docs
- * for the constructor and the class' methods
+ * A custom GUI component that displays a summary of a guest's order in the 
+ * guest-facing GUI.
+ *
+ * This panel shows the guest's name, the total price of their order, and a 
+ * list of ordered menu items.
+ *
  * 
- * 
- * @author Christoffer Søndergaard
- * @version 21/05/2025 - 08:36
+ * Author: Christoffer Søndergaard  
+ * Version: 08/06/2025 - 13:20
  */
 public class ComponentGuestOrderSummary extends JPanel
 {
-	public ComponentGuestOrderSummary(String guestName, double price, List<String> items)
+    /**
+     * Creates a new ComponentGuestOrderSummary with the given guest name, 
+     * total price, and list of menu items.
+     *
+     * @param guestName the name of the guest/customer who the order is being summarized for
+     * @param price the total price of the guest's order
+     * @param items a list of item names representing the guest's order contents
+     */
+	public ComponentGuestOrderSummary(String guestName, double price, List<String> menuItems)
 	{
 		// Set the layout manager to BorderLayout
 		setLayout(new BorderLayout());
@@ -76,18 +98,22 @@ public class ComponentGuestOrderSummary extends JPanel
 		// Use vertical layout
 		itemPanel.setLayout(new BoxLayout(itemPanel, BoxLayout.Y_AXIS));
 		
-		// Make the items panel transparent
+		// Make the menuItems panel transparent
 		itemPanel.setOpaque(false);
 		
-		// Add top padding (10px) to create space between name and items
+		// Add top padding (10px) to create space between name and menuItems
 		itemPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
 
 		// Loop through the list of item strings
-		for (String item : items)
+		for (String menuitem : menuItems)
 		{
 			// Create a label for each item with italic font
-			JLabel lblItem = new JLabel("1x " + item);
+			JLabel lblItem = new JLabel("1x " + menuitem);
+			
+			// Sets the font to the specified style type and size
 			lblItem.setFont(new Font("SansSerif", Font.ITALIC, 16));
+			
+			// Changes the color of the text to dark grey
 			lblItem.setForeground(new Color(70, 70, 70));
 			
 			// Add the item label to the panel
