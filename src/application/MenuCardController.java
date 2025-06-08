810d4f6672a2b59084dd9d7501adbc7f33e536c1
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-08 16:37:17 +0200

    Changed: MenuCardController (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/application/MenuCardController.java
+++ src/application/MenuCardController.java
@@ -1,5 +1,6 @@
 package application;
 
+// Imports
 import java.sql.SQLException;
 import java.util.List;
 
@@ -8,26 +9,37 @@ import database.MenuCardDB;
 import database.MenuCardImpl;
 import model.MenuCard;
 
+
 /**
- * a controller class that bridges the logic from the model layer and the user interface from the gui layer
- * 
- * @author Anders Trankjær
- * @version 2025/13/05/13:10
+ * The MenuCardController acts as a bridge between the GUI layer and the data access layer (DAO).
+ * The controller coordinates the requests and responses related to MenuCard entities, by delegating
+ * the database operations to the DAO implementation MenuCardDB.
+ *
+ * This separation helps to ensure low coupling between GUI and data persistence, and thereby improve
+ * the system's maintainability and possibility to scale.
+ *
+ *
+ * @author Anders Trankjær & Christoffer Søndergaard
+ * @version 07/06/2025 - 17:16
  */
 public class MenuCardController
 {
-
-	/**
-	 * this method asks the Database to retrieve a list of MenuCard object from a restaurant with a given restaurantCode
-	 * 
-	 * @param restaurantCode - the searchcriteria which we look for a restaurant with
-	 * @return a list of all MenuCards from a specific restaurant.
-	 * @throws DataAccessException
-	 * @throws SQLException
-	 */
+    /**
+     * Retrieves a list of MenuCard objects from a restaurant with a given restaurantCode
+     *
+     * The controller delegates the data retrieval to the DAO layer, hiding database logic from the GUI.
+     *
+     * @param restaurantCode the unique code used to identify a specific restaurant
+     * @return a list of MenuCard instances associated with the given restaurant
+     * @throws DataAccessException if a database access issue occurs somewhere at the DAO level
+     * @throws SQLException if an SQL query execution fails
+     */
 	public List<MenuCard> findMenuCardsByRestaurantCode(String restaurantCode) throws DataAccessException, SQLException
 	{
-		MenuCardImpl dao = new MenuCardDB();
-		return dao.findMenuCardsByRestaurantCode(restaurantCode);
+		// Creates an instance of the DAO interface using the concrete MenuCardDB class implementation
+		MenuCardImpl dataAccessObject = new MenuCardDB();
+	
+		// Calls upon the DAO method to retrieve a list of menucard objects from the Restaurant with the specified code
+		return dataAccessObject.findMenuCardsByRestaurantCode(restaurantCode);
 	}
 }
