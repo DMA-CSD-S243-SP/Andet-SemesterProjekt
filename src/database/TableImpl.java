2c623fa0eca3c7f3a607a7ef8e3374321771fbc7
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-08 17:45:24 +0200

    Changed: TableImpl (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/database/TableImpl.java
+++ src/database/TableImpl.java
@@ -1,13 +1,26 @@
 package database;
 
+// Imports
 import java.sql.SQLException;
 
 import model.Table;
 
+
 /**
+ * An interface that defines a contract for accessing TableDB,
+ * specifically for finding a restaurant by its code.
+ *
+ * This interface is part of the DAO (Data Access Object) design pattern and specifies the methods
+ * required to retrieve data from the underlying data source.
+ * 
+ * The contract ensures consistency across all classes that implements the interface.
+ * It also guarantees that certain functionality is available and any class that 
+ * implements TableImpl must provide the one method called:
+ * findTableByCode.
+ * 
  * 
  * @author Anders Trankjær & Christoffer Søndergaard
- * @version 02/06/2025 - 13:54
+ * @version 07/06/2025 - 16:48
  */
 public interface TableImpl
 {
