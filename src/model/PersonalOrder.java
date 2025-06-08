707c9480d9bbef3c3d0fb8ee580a9304029ee9df
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-08 17:51:49 +0200

    Changed: PersonalOrder (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/model/PersonalOrder.java
+++ src/model/PersonalOrder.java
@@ -5,6 +5,7 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  * Represents a guest’s individual and personal order, at a Bone’s restaurant.
  * 
@@ -23,18 +24,10 @@ import java.util.List;
  * 
  * 
  * @author Line Bertelsen & Christoffer Søndergaard
- * @version 11-05-2025 - 21:01
+ * @version 07-06-2025 - 20:19
  */
 public class PersonalOrder
 {
-// OLD - 	private double totalPersonalOrderPrice;
-	/*
-	 * // private List<SelectionOption> listOfSelectionOption; // private
-	 * List<AddOnOption> listOfAddOnOptions; //Line: Attributes i've added private
-	 * TableOrder tableOrder; private MainCourse mainCourse; private MenuItem
-	 * menuItem;
-	 */
-
 	// Attributes/Instance variables
 	private int PersonalOrderId;
 	private int customerAge;
@@ -44,157 +37,182 @@ public class PersonalOrder
 	private List<Discount> listOfAllDiscounts;
 	private List<PersonalOrderLine> personalOrderLineList;
 
-	// Constructor of PersonalOrder
+	
+	/**
+	 * Constructs a new PersonalOrder instance that is associated with the specified 
+	 * TableOrder object.
+	 * 
+	 * When a PersonalOrder is created, it initializes an empty list of PersonalOrderLine 
+	 * objects and an empty list of applied Discount objects.
+	 * These lists are used to store the guest's selected menu items and any of the possible
+	 * applicable discounts.
+	 * 
+	 * @param tableOrder the TableOrder that this PersonalOrder is part of
+	 */
 	public PersonalOrder(TableOrder tableOrder)
 	{
 		// Instantiates the list of personal order lines and the list of discounts
 		this.personalOrderLineList = new ArrayList<>();
 		this.listOfAllDiscounts = new ArrayList<>();
 	}
-
+	
+	
 	/**
 	 * The get method returns the value of the variable customerAge
 	 * 
-	 * @return customerAge
+	 * @return customerAge the age of the customer
 	 */
 	public int getCustomerAge()
 	{
 		return customerAge;
 	}
 
+	
 	/**
 	 * The set method takes a parameter customerAge and assigns it to the
 	 * this.customerAge variable.
 	 * 
-	 * @param customerAge
+	 * @param customerAge the age of the customer
 	 */
 	public void setCustomerAge(int customerAge)
 	{
 		this.customerAge = customerAge;
 	}
 
+	
 	/**
 	 * The get method returns the value of the variable customerName
 	 * 
-	 * @return customerName
+	 * @return customerName the name of the customer
 	 */
 	public String getCustomerName()
 	{
 		return customerName;
 	}
 
+	
 	/**
 	 * The set method takes a parameter customerName and assigns it to the
 	 * this.customerName variable.
 	 * 
-	 * @param customerName
+	 * @param customerName the name of the customer
 	 */
 	public void setCustomerName(String customerName)
 	{
 		this.customerName = customerName;
 	}
 
+	
 	/**
-	 * The addAllDiscount method uses a foreach-loop and the method addDiscount to
-	 * add all discounts objects to the list listOfAllDiscounts
+	 * Adds a list of Discount objects to this PersonalOrder instance.
+	 * This method uses the addDiscount method to add each discount to
+	 * the listOfAllDiscounts individually.
+	 *
+	 * @param listOfAllDiscounts the list of Discount objects to add
 	 */
 	public void addAllDiscounts(List<Discount> listOfAllDiscounts)
 	{
+		// Uses a for-each loop to iterate through all the Discount objects in the listOfAllDiscounts
 		for (Discount discount : listOfAllDiscounts)
 		{
+			// Adds a Discount object to the list of Discount objects
 			addDiscount(discount);
 		}
 	}
 
+	
 	/**
 	 * The method addDiscount add one discount object to the list listOfAllDiscounts
 	 * 
-	 * @param discount
+	 * @param discount the discount object to add to the list of discounts
 	 */
 	public void addDiscount(Discount discount)
 	{
 		this.listOfAllDiscounts.add(discount);
 	}
 
+	
 	/**
 	 * The method removeDiscount removes a discount object from the list
 	 * listOfAllDiscounts
 	 * 
-	 * @param discount
+	 * @param discount the discount object to remove from the list of discounts
 	 */
 	public void removeDiscount(Discount discount)
 	{
 		this.listOfAllDiscounts.remove(discount);
 	}
 
+	
 	/**
-	 * The clearDiscont method uses a foreach-loop and the method removeDiscount to
-	 * remove all discounts objects to the list listOfAllDiscounts
+	 * Removes all discounts that are associated with this PersonalOrder instance.
+	 *
+	 * This clears the listOfAllDiscounts by individually removing each Discount
+	 * object individually from the list of discounts.
 	 */
-	public void clearDiscont()
+	public void clearDisconts()
 	{
+		// Uses a for-each loop to iterate through all the Discount objects in the listOfAllDiscounts
 		for (Discount discount : listOfAllDiscounts)
 		{
+			// Removes a Discount object from the list of Discount objects
 			removeDiscount(discount);
 		}
 	}
 
+	
 	/**
 	 * The method addMainCourseLine, adds a main course, a list of
 	 * listOfAddOnOptions and a list of listOfSelectionOption to personalOrder
 	 * 
-	 * @param mainCourse
-	 * @param listOfAddOnOptions
-	 * @param listOfSelectionOption
+	 * @param mainCourse the MainCourse object to associate this OrderLine with
+	 * @param listOfAddOnOptions the list of AddOn options for this MainCourse
+	 * @param listOfSelectionOption the list of Selection options for this MainCourse
 	 */
-	public void addMainCourseLine(MainCourse mainCourse, List<AddOnOption> listOfAddOnOptions,
-			List<SelectionOption> listOfSelectionOption)
+	public void addMainCourseLine(MainCourse mainCourse, List<AddOnOption> listOfAddOnOptions, List<SelectionOption> listOfSelectionOption)
 	{
-		// Dummy Code Block
-
-		/*
-		 * //Add a single mainCourse to the MainCourseLine this.mainCourse = mainCourse;
-		 * 
-		 * //Adds list of addOnOptions to the list listOfAddOnOptions
-		 * this.listOfAddOnOptions.addAll(listOfAddOnOptions);
-		 * 
-		 * //Adds list of addOnOptions to the list listOfAddOnOptions
-		 * this.listOfSelectionOption.addAll(listOfSelectionOption);
-		 * 
-		 * //PersonalOrderLine.addOnOption(); //PersonalOrderLine.addSelectionOption();
-		 */
-		
+		// Creates a PersonalOrderLine object and stores it within the mainCourseLine variable
 		PersonalOrderLine mainCourseLine = new PersonalOrderLine(mainCourse);
-		for (AddOnOption option: listOfAddOnOptions)
+
+		// Uses a for-each loop to iterate through the supplied listOfAddonOptions
+		for (AddOnOption addonOption: listOfAddOnOptions)
 		{
-			mainCourseLine.addAddOnOption(option);
+			// Adds the addOnOption to the mainCourseLine variable's PersonalOrderLine instance
+			mainCourseLine.addAddOnOption(addonOption);
 		}
-		for (SelectionOption option: listOfSelectionOption)
-		{
-			mainCourseLine.addSelectionOption(option);
-		}
-		addPersonalOrderLine(mainCourseLine);
 		
+		// Uses a for-each loop to iterate through the supplied listOfSelectionOption
+		for (SelectionOption selectionOption: listOfSelectionOption)
+		{
+			// Adds the selection option to the mainCourseLine variable's PersonalOrderLine instance
+			mainCourseLine.addSelectionOption(selectionOption);
+		}
+		
+		// Adds the PersonalOrderLine object containing the mainCourseLine information to the PersonalOrder
+		addPersonalOrderLine(mainCourseLine);
 	}
 
+	
 	/**
-	 * Adds a all menuItems to the personal order Like potatoDish and sideOrderItem
+	 * Adds a PersonalOrderLine object to the list of PersonalOrderLines
 	 * 
-	 * @param menuItem
+	 * @param PersonalOrderLine the PersonalOrderLine object to add to the list of PersonalOrderLine objects
 	 */
 	public void addPersonalOrderLine(PersonalOrderLine personalOrderLine)
 	{
 		this.personalOrderLineList.add(personalOrderLine);
 	}
 
+	
 	/**
-	 * The clearDiscont method uses a foreach-loop and the method removeDiscount to
-	 * remove all discounts objects to the list listOfAllDiscounts
+	 * The clearMenuItemLine method simply instantiates a new and empty ArrayList 
+	 * to clear the contents of the PersonalOrderLineList 
 	 */
 	public void clearMenuItemLine()
 	{
 		personalOrderLineList = new ArrayList<>();
 	}
+	
 
 	/**
 	 * Calculates and returns the total price of all the PersonalOrderLines that are
@@ -217,31 +235,39 @@ public class PersonalOrder
 		// it also keeps track of whether a premium potato has been found.
 		boolean isPremiumPotato = false;
 
-		// Iterates through the list of personal order lines
+		// Uses a for-each loop to iterate through the list of personal order lines
 		for (PersonalOrderLine personalOrderLine : personalOrderLineList)
 		{
-			// Adds the current PersonalOrderLine instances' price to the current value of
-			// the totalPrice
+			// Adds the current PersonalOrderLine instances' price to the 
+			// current value of the totalPrice
 			totalPrice = totalPrice + personalOrderLine.getPersonalOrderLineLunchPrice();
 
-			// Sets
+			// If the personalOrderLine contains a premium potato dish then execute this section
 			if (personalOrderLine.isPremiumPotatoDish())
 			{
+				// Sets the isPremiumPotato variable to true
 				isPremiumPotato = true;
 			}
 		}
-		// If one or more premium potatos have been found add the price of a premium
+		
+		// If one or more premium potatos have been found then add the price of a premium
 		// potato to the order.
 		if (isPremiumPotato)
 		{
-			totalPrice = totalPrice + 20.; // TODO: Replace this hardcoded value with a reference to a static
-											// premiumPotatoPrice
+			// Adds +20 to the price for having ordered a premium potato dish
+			totalPrice = totalPrice + 20.0; 
+			
+			// TODO: Replace this hardcoded value with a reference to a static
+			// premiumPotatoPrice
 		}
 
 		// TODO: Later add calculations for the various types of discount when that use
 		// case is being handled
+		
+		// Returns the total price during lunch hours
 		return totalPrice;
 	}
+	
 	
 	/**
 	 * Calculates and returns the total price of all the PersonalOrderLines that are
@@ -264,58 +290,101 @@ public class PersonalOrder
 		// it also keeps track of whether a premium potato has been found.
 		boolean isPremiumPotato = false;
 
-		// Iterates through the list of personal order lines
+		// Uses a for-each loop to iterate through the list of personal order lines
 		for (PersonalOrderLine personalOrderLine : personalOrderLineList)
 		{
 			// Adds the current PersonalOrderLine instances' price to the current value of
 			// the totalPrice
 			totalPrice = totalPrice + personalOrderLine.getPersonalOrderLineEveningPrice();
 
-			// Sets
+			// If the personalOrderLine contains a premium potato dish then execute this section
 			if (personalOrderLine.isPremiumPotatoDish())
 			{
+				// Sets the isPremiumPotato variable to true
 				isPremiumPotato = true;
 			}
 		}
-		// If one or more premium potatos have been found add the price of a premium
+		
+		// If one or more premium potatos have been found then add the price of a premium
 		// potato to the order.
 		if (isPremiumPotato)
 		{
-			totalPrice = totalPrice + 20.; // TODO: Replace this hardcoded value with a reference to a static
-											// premiumPotatoPrice
+			// Adds +20 to the price for having ordered a premium potato dish
+			totalPrice = totalPrice + 20.0; 
+			
+			// TODO: Replace this hardcoded value with a reference to a static
+			// premiumPotatoPrice
 		}
 
 		// TODO: Later add calculations for the various types of discount when that use
 		// case is being handled
+		
+		// Returns the total price during evening hours
 		return totalPrice;
 	}
 
+	
+	/**
+	 * Adds a MenuItem object to the list of PersonalOrderLines as a single line item.
+	 *
+	 * @param menuItem the menu item to add to this guest's personal order
+	 */
 	public void addMenuItemLine(MenuItem menuItem) 
 	{
 		PersonalOrderLine menuItemLine = new PersonalOrderLine(menuItem);
 		addPersonalOrderLine(menuItemLine);
 	}
 	
+	
+	/**
+	 * Retrieves a copy of the list containing all PersonalOrderLine objects in this personal order.
+	 *
+	 * @return a new List containing the current PersonalOrderLine instances
+	 */
 	public List<PersonalOrderLine> getPersonalOrderLines()
 	{
 		return new ArrayList<>(personalOrderLineList);
 	}
 	
+	
+	/**
+	 * Retrieves the names of all menu items in this personal order.
+	 *
+	 * @return a list of Strings, each representing the name of a MenuItem in the order
+	 */
 	public List<String> getNameOfItemsInList()
 	{
-		List<String> nameList = new ArrayList<String>();
-		for (PersonalOrderLine pList : personalOrderLineList)
-		{
-			nameList.add(pList.getMenuItem().getName());
-		}
-		return nameList;
+	    // Instantiates a new list to store the names of the MenuItems
+	    List<String> listOfMenuItemNames = new ArrayList<String>();
+
+	    // Uses a for-each loop to iterate through the list of personal order lines
+	    for (PersonalOrderLine personalOrderLine : personalOrderLineList)
+	    {
+	        // Retrieves the name of the MenuItem from the current PersonalOrderLine and adds it to the list of menu item names
+	    	listOfMenuItemNames.add(personalOrderLine.getMenuItem().getName());
+	    }
+
+	    // Returns the list of MenuItem names
+	    return listOfMenuItemNames;
 	}
 	
+	
+	/**
+	 * Returns the unique ID associated with this PersonalOrder.
+	 *
+	 * @return the ID of this personal order
+	 */
 	public int getPersonalOrderId()
 	{
 		return this.PersonalOrderId;
 	}
 	
+	
+	/**
+	 * Sets the unique ID for this PersonalOrder.
+	 *
+	 * @param personalOrderId the unique identifier to assign to this personal order
+	 */
 	public void setPersonalOrderId(int personalOrderId)
 	{
 		this.PersonalOrderId = personalOrderId;
