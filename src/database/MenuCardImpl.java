623a135906c8483402368b28e916798b58ccf8b7
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-08 17:46:41 +0200

    Changed: MenuCardImpl (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.

--- src/database/MenuCardImpl.java
+++ src/database/MenuCardImpl.java
@@ -1,22 +1,28 @@
 package database;
 
+// Imports
 import java.sql.SQLException;
 import java.util.List;
 
 import model.AvailabilityTracker;
 import model.MenuCard;
 
+
 /**
- * An interface that defines a contract for accessing MenuCardImpl,
+ * An interface that defines a contract for accessing MenuCardDB,
  * specifically for finding a list of menuCards and availabilityTacker based on its Code/ID.
  * 
+ * This interface is part of the DAO (Data Access Object) design pattern and specifies the methods
+ * required to retrieve data from the underlying data source.
+ * 
  * The contract ensures consistency across all classes that implement the interface. 
- * It guarantees that certain functionality is available 
- * and any class that implements MenuItemImpl must provide a two methods called 
+ * It also guarantees that certain functionality is available and any class that
+ * implements MenuCardImpl must provide a two methods called:
  * findMenuCardsByRestaurantCode and findAvailabilityTrackerById.
  * 
- * @author Line Bertelsen
- * @version 02/06/2025 - 16.02
+ * 
+ * @author Line Bertelsen & Christoffer Søndergaard
+ * @version 07/06/2025 - 15:55
  */
 public interface MenuCardImpl
 {
@@ -32,6 +38,7 @@ public interface MenuCardImpl
 	 * @throws SQLException			- if a SQL operation fails
 	 */
 	List<MenuCard> findMenuCardsByRestaurantCode(String restaurantCode) throws DataAccessException, SQLException;
+	
 	
 	/**
      * Method creates a list of shallow clone of the AvailabilityTrackers that has the given unique id. 
