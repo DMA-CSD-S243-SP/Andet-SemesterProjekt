1c28d96aa6bd1a233f1e043fbcc126fad742fe9e
Christoffer <christoffer.soendergaard@outlook.com>
2025-06-09 14:09:32 +0200

    Changed: Main (Code Review)
    Performed code review on the class to find and correct potential errors, and enforce the coding standard.
    
    Also made changes to the JavaDocs to reflect changes and inaccuracies in descriptiveness.
    
    Also turned the order of which the windows are launched around, so that the guest window is the one in front.

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -3,32 +3,46 @@ package gui;
 
 
 /**
- * TODO: Write a thorough description of this class and also java docs
- * for the constructor and the class' methods
+ * The Main class serves as the entry point for the system.
+ * 
+ * This class initializes and displays two primary views:
+ * - ViewStaffLogin: which is a log-in view for the restaurant's staff
+ * - ViewGuestTableInformation which is a view that lets guests enter their table number and start their ordering flow
+ * 
+ * The main method launches both GUIs, making them available for user interaction
+ * and showcasing a demo, in a final product these two graphical user interfaces
+ * would be clearly separated.
  * 
  * 
- * @author Christoffer Søndergaard
- * @version 21/05/2025 - 12:32
- */	
+ * Author: Christoffer Søndergaard  
+ * @version: 09/06/2025 - 14:04
+ */
 public class Main
 {
+    /**
+     * The main method that starts the system.
+     * 
+     * This method creates and displays the staff's login view and the
+     * customers' table information information view.
+     * 
+     * @param args command-line arguments
+     */
 	public static void main(String[] args)
 	{
 		// Tries to run the code within the braces
 		try
 		{
-			// Creates a ViewGuestTableInformation instance and stores it within the viewGuestTableInformation variable
-			ViewGuestTableInformation  viewGuestTableInformation = new ViewGuestTableInformation();
-			
-			
-			
-			// Changes the visibility of the LoginView window to be visible
-			viewGuestTableInformation.setVisible(true);
-			
+			// Creates a ViewStaffLogin instance and stores it within the viewStaffLogin variable
 			ViewStaffLogin viewStaffLogin = new ViewStaffLogin();
 
 			// Changes the visibility of the LoginView window to be visible
 			viewStaffLogin.setVisible(true);
+			
+			// Creates a ViewGuestTableInformation instance and stores it within the viewGuestTableInformation variable
+			ViewGuestTableInformation  viewGuestTableInformation = new ViewGuestTableInformation();
+			
+			// Changes the visibility of the LoginView window to be visible
+			viewGuestTableInformation.setVisible(true);
 		}
 		
 		// Catches any type of exception that may occur while running the above code
